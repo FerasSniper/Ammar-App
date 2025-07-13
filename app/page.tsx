4e2b6282327199cdf22bb28901373fd4a896cc62
FerasSniper <s202038040@kfupm.edu.sa>
2025-07-13 11:29:15 +0300

    Initial Next.js scaffold

--- app/page.tsx
+++ app/page.tsx
@@ -23,6 +23,9 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+          <li className="tracking-[-.01em]">
+            This is the projct Setup for Summer Training T243 KFUPM!
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
